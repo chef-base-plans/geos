fd96bde93ef16950f4ae0429f7cf45e148a7dd86
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 17:10:10 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/geos_works.rb
+++ controls/geos_works.rb
@@ -1,24 +1,28 @@
 title 'Tests to confirm geos works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'geos')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
 control 'core-plans-geos-works' do
   impact 1.0
   title 'Ensure geos works as expected'
   desc '
+  Verify geos by ensuring (1) its installation directory exists and (2) that
+  it returns the expected version
   '
-  geos_path = command("hab pkg path #{plan_ident}")
-  describe geos_path do
+  
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
   
-  geos_pkg_ident = ((geos_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  describe command("DEBUG=true; hab pkg exec #{ geos_pkg_ident} geos-config --version") do
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} geos-config --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /3.7.1/ }
+    its('stdout') { should match /#{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
 end

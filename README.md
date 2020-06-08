8e50cc9057f05913d652217bac57766b6dba53e1
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 17:10:10 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_apis/build/status/chef-base-plans.geos?branchName=master)](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_build/latest?definitionId=69&branchName=master)
+
 # geos
 
 GEOS (Geometry Engine - Open Source) is a C++ port of the ​Java Topology Suite (JTS).
--- controls/geos_exists.rb
+++ controls/geos_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm geos exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'geos')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-geos_relative_path = input('command_path', value: '/bin/geos-config')
-geos_installation_directory = command("hab pkg path #{plan_ident}")
-geos_full_path = geos_installation_directory.stdout.strip + "#{ geos_relative_path}"
- 
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+command_relative_path = input('command_relative_path', value: '/bin/geos-config')
+command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+
 control 'core-plans-geos-exists' do
   impact 1.0
   title 'Ensure geos exists'
   desc '
-  '
-   describe file(geos_full_path) do
+  Verify geos by ensuring /bin/geos-config exists'
+  
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  describe file(command_full_path) do
     it { should exist }
   end
 end
